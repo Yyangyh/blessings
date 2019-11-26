5b83b65f48ed8dbf0796842a198d7acc4a0fbf71
yulanyue <13433162425@163.com>
2019-11-26 16:16:55 +0800

    11.26

---
+++ pages/activity/apply.vue
@@ -0,0 +1,162 @@
+<template>
+	<view class="content">
+		<view class="status_bar">
+			
+		</view>
+		<returns :titles='title'></returns>
+		<view class="activitys">
+			<image src='../../static/image/index/sm.png'></image>
+			<view class="a-right">
+				<view class="theme">让爱回家|高分孩子·智慧父母一大型公益巡讲</view>
+				<view class="line">
+				  <image src="../../static/image/index/apply.png"></image>
+				  <text>已报名37人</text>
+				  <text>限1000人报名</text>
+				 </view>
+				 <view class="price">￥100.0/399积分</view>
+			</view>
+		</view>
+		<view class="information">
+			<form>
+				<view class=""><text>姓名：</text><input type="text" placeholder="请输入姓名" v-model="name"></view>
+				<hr />
+				<view class=""><text>手机号码：</text><input type="text" placeholder="请输入手机号码" v-model="phone" value></view>
+				<hr />
+				<view class=""><text>备注：</text><input type="text" placeholder="请输入备注"></view>
+				<hr />
+			</form>
+			<view>
+				<button form-type="submit" @click="register()">提交</button>
+			</view>
+		</view>
+		<!-- 提交成功弹框 -->
+		<view class="k">
+			提交成功
+		</view>
+	</view>
+</template>
+
+<script>
+	import returns from '../common/returns.vue'
+	export default{
+		components:{
+			returns
+		},
+		data(){
+			return{
+				title:'立即报名',
+				name:'',
+				phone:''
+			}
+		},
+		methods:{
+			 register(){
+				  let tel = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
+				  let tel2 = tel.test(this.phone);
+				   if (this.phone == "" || this.name == "") {
+					  alert( "输入框不能为空!");
+				  }else if(!tel2){
+					  alert( "请输入正确的11位手机号码");
+				  }
+			 }
+		}
+	}
+</script>
+
+<style lang="less">
+	.content{
+		width: 100%;
+		height: 100vh;
+		background-color: #F6F6F6;
+		position: relative;
+		.activitys{
+			background-color: #FFFFFF;
+			padding: 31rpx 25rpx 31rpx 31rpx;
+			display: flex;
+			image{
+				width: 275rpx;
+				height: 177rpx;
+				margin: 31rpx 31rpx 31rpx 0;
+			}
+			.a-right{
+				.theme{
+					font-size: 24upx;
+					color:#333333;
+					margin:20upx 0 0upx 0;
+				}
+				.line{
+					display: flex;
+					align-items: center;
+					image{
+						width: 32rpx;
+						height: 32rpx;
+						margin-left: -5rpx;
+					}
+					text{
+						font-size: 28rpx;
+						color:#666666;
+						margin-right: 6rpx;
+					}
+					.theme1{
+						font-size: 24upx;
+						color:#333333;
+					}
+				}
+				.price{
+					font-size: 25rpx;
+					font-weight: bold;
+					color:#D80000;
+					letter-spacing: 3rpx;
+				}
+			}
+		}
+		.information{
+			background-color: #FFFFFF;
+			width: 100%;
+			height:70vh;
+			padding: 40rpx;
+			margin: 30rpx 0;
+			hr{
+				height:1rpx;
+				border: none;
+				border-top:1px solid #F4F4F4;
+				width: 96%;
+				margin: 0 100rpx 0 0;
+			}
+			view{
+				display: flex;
+				font-size: 28rpx;
+				margin: 20rpx 0;
+				align-items: center;
+			}
+			button{
+				width: 600rpx;
+				height: 80rpx;
+				font-size: 30rpx;
+				color: #FFFFFF;
+				text-align: center;
+				line-height: 80rpx;
+				background-color: #D80000;
+				border: none;
+				border-radius: 40rpx;
+				letter-spacing: 10rpx;
+				margin: 30rpx 2rpx 0 28rpx;
+				
+			}
+		}
+		.k{
+			width: 254rpx;
+			height: 106rpx;
+			color: #FFFFFF;
+			background:rgba( 0,0,0,.5);
+			text-align: center;
+			line-height: 106rpx;
+			position: absolute;
+			top: 400rpx;
+			left: 0;
+			right: 0;
+			margin: auto;
+			display: none;
+		}
+	}
+</style>
---
+++ pages/activity/particulars.vue
@@ -0,0 +1,251 @@
+<template>
+	<view class="content">
+		<view class="status_bar">
+			
+		</view>
+		<returns :titles='title'></returns>
+		<view class="worp">
+			<image src="../../static/image/index/huo1.png"></image>
+			<view class="theme">让爱回家|高分孩子·智慧父母一大型公益巡讲</view>
+			<view class="line">
+			  <image src="../../static/image/index/apply.png"></image>
+			  <text>已报名37人</text>
+			  <text>限1000人报名</text>
+			 </view>
+			 <view class="price">￥100.0/399积分</view>
+			<view class="count">
+				 <text>剩余</text>
+				 <view>00</view>
+				 <text>天</text>
+				 <view>01</view>
+				 <text>时</text>
+				 <view>34</view>
+				 <text>分</text>
+				 <view>48</view>
+				 <text>秒</text>
+			 </view>
+		</view>
+		<view class="worp">
+			<view class="line">
+			  <image src="../../static/image/index/time.png"></image>
+			  <text class="theme1">6月27日 </text>
+			  <text>星期三</text>
+			  <text>17:00-19:00</text>
+			</view>
+			<hr />
+			<view class="line">
+				<image src='../../static/image/index/site.png'></image>
+				<text class="theme1">广东省东莞市帝京国际酒店</text>
+			</view>
+			<hr />
+			<view class="line">
+				<image src='../../static/image/index/zhubanfang.png'></image>
+				<text class="theme1">主办方：五福家庭教育</text>
+			</view>
+		</view>
+		<view class="worp">
+			<view class="top">
+				<text>已报名</text>
+				<text>查看更多</text>
+			</view>
+			<view class="personnel-list">
+				<view class="list">
+					<image src='../../static/image/index/per.png'></image>
+					<view>老胡</view>
+				</view>
+			</view>
+		</view> 
+		<view class="p-t">活动详情</view>
+		<image class="image" src='../../static/image/index/xq.png'></image>
+		<view class="underway">
+			<button type="default" @tap="service.jump('./apply')">立即报名</button>
+			<button type="default">邀请好友</button>
+		</view>
+		<view class="finish">
+			<button>活动已结束</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	import returns from '../common/returns.vue'
+	export default{
+		components:{
+			returns
+		},
+		data(){
+			return{
+				title:'活动详情',
+			}
+		},
+		methods:{
+			// toapply(){
+			// 	uni.navigateTo({
+			// 		url:'/pages/activity/apply',
+			// 	})
+			// },
+		}
+	}
+</script>
+
+<style lang='scss'>
+	.content{
+		height: 100%vh;
+		width: 100%;
+		background-color: #F2F2F2;
+		.worp{
+			width: 100%;
+			background-color: #FFFFFF;
+			padding-top: 0 !important;
+			margin:0 0 30rpx 0 ;
+			hr{
+				height:1rpx;
+				border: none;
+				border-top:1px solid #F4F4F4;
+				width: 97%;
+				margin: 0 auto;
+			}
+			image{
+				width: 100%;
+				height: 364rpx;
+			}
+			.theme{
+				font-size: 24upx;
+				color:#333333;
+				margin:20upx 0 20upx 23upx;
+			}
+			.line{
+				display: flex;
+				align-items: center;
+				margin-left: 23rpx;
+				image{
+					width: 32rpx;
+					height: 32rpx;
+					margin-right: 28rpx;
+				}
+				text{
+					font-size: 28rpx;
+					color:#666666;
+					margin-right: 20rpx;
+				}
+				.theme1{
+					font-size: 24upx;
+					color:#333333;
+					margin:20upx 0 20upx 0upx;
+				}
+			}
+			.price{
+				font-size: 25rpx;
+				font-weight: bold;
+				color:#D80000;
+				margin: 20rpx 0 0 23rpx;
+				letter-spacing: 3rpx;
+			}
+			.count{
+				display: flex;
+				margin-left: 23rpx;
+				align-items: center;
+				text{
+					font-size: 26rpx;
+					color:#F2221F;
+				}
+				view{
+					width: 44rpx;
+					height: 44rpx;
+					text-align: center;
+					font-size: 21rpx;
+					color: #FFFFFF;
+					line-height: 44rpx;
+					background-color: #D80000;
+					border-radius:5rpx;
+					margin: 10rpx 0;
+				}
+			}
+			.top{
+				display: flex;
+				justify-content: space-between;
+				align-items:center;
+				margin: 0 23rpx;
+				padding: 20rpx 0;
+				text:first-child{
+					font-size: 27rpx;
+					color: #000000;
+				}
+				text:last-child{
+					font-size: 23rpx;
+					color:#EF7C38;
+				}
+			}
+			.personnel-list{
+				display: flex;
+				justify-content: space-around;
+				.list{
+					image{
+						width: 96rpx;
+						height: 96rpx;
+						border-radius: 50%;
+					}
+					view{
+						font-size: 23rpx;
+						text-align: center;
+					}
+				}
+			}
+		}
+		.p-t{
+			font-size: 28rpx;
+		}
+		.image{
+			margin-top: 30rpx;
+			width: 100%;
+			height: 988rpx;
+		}
+		.underway{
+			background: #FFFFFF;
+			display: flex;
+			padding: 30rpx 0;
+			button:first-child{
+				width: 250rpx;
+				height: 80rpx;
+				font-size: 30rpx;
+				color: #FFFFFF;
+				text-align: center;
+				line-height: 80rpx;
+				background-color: #D80000;
+				border: none;
+				border-radius: 40rpx;
+				letter-spacing: 10rpx;
+			}
+			button:last-child{
+				width: 250rpx;
+				height: 80rpx;
+				font-size: 30rpx;
+				color: #FFFFFF;
+				text-align: center;
+				line-height: 80rpx;
+				background-color: #EF7C38;
+				border: none;
+				border-radius: 40rpx;
+				letter-spacing: 10rpx;
+			}
+		}
+	/* 	结束按钮 */
+		.finish{
+			background: #FFFFFF;
+			padding: 30rpx 0;
+			display: none;
+			button{
+				width: 600rpx;
+				height: 80rpx;
+				font-size: 30rpx;
+				color: #FFFFFF;
+				text-align: center;
+				line-height: 80rpx;
+				background-color: #999999;
+				border: none;
+				border-radius: 40rpx;
+				letter-spacing: 10rpx;
+			}
+		}
+	}
+</style>
