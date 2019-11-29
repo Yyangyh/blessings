45b0dc2c4278e6a480a7d242954f7e21b237ec14
yulanyue <13433162425@163.com>
2019-11-29 18:27:00 +0800

    11.29

--- pages/subclass/applyFor.vue
+++ pages/subclass/applyFor.vue
@@ -8,21 +8,21 @@
 			<view class="write">
 				<view class="msg">
 					<text>联系人</text><text class="t2">*</text>
-					<input type="text" placeholder="您的名字">
+					<input type="text" placeholder="您的名字" v-model="name">
 				</view>
 				<hr />
 				<view class="msg">
 					<text>手机号</text><text class="t2">*</text>
-					<input type="text" placeholder="您的手机号码">
+					<input type="text" placeholder="您的手机号码" v-model="phone">
 				</view>
 				<hr />
-				<view>请仔细填写联系方式，保证我们能尽快联系到您~</view>
+				<view class="t3">请仔细填写联系方式，保证我们能尽快联系到您~</view>
 			</view>
 			
 			<view class="write">
 				<view class="msg">
 					<text>身份证号</text><text class="t2">*</text>
-					<input type="text" placeholder="填写您的身份证号">
+					<input type="text" placeholder="填写您的身份证号" v-model="idCard">
 				</view>
 			</view>
 			<view class="write">
@@ -37,7 +37,7 @@
 			<view class="write">
 				<view class="msg">
 					<text>文化程度</text><text class="t2">*</text>
-					<input type="text" placeholder="请填写文字">
+					<input type="text" placeholder="请填写文字" v-model="text">
 				</view>
 				<hr />
 				<view class="msg1">
@@ -58,9 +58,10 @@
 				<hr />
 				<view class="msg2">
 					<text class="t1">收货地址</text><text class="t2">*</text>
-					<textarea cols='2'rows="16" value="" placeholder="请填写详细地址" />
+					<textarea rows="2" cols="10" value="" placeholder="请填写详细地址" v-model="site"></textarea>
 				</view>
 			</view>
+			<button type="default"  @click="register()">立即申请</button>
 		</form>
 	</view>
 </template>
@@ -73,11 +74,56 @@
 		},
 		data(){
 			return{
-				title:'申请'
+				title:'申请',
+				name:'',
+				phone:'',
+				idCard:'',
+				text:'',
+				site:'',
 			}
 		},
 		methods:{
-			
+			 register(){
+				
+				 let tel2 = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/.test(this.phone);
+				 let idcard =  /^\d{15}(\d{2}[A-Za-z0-9])?$/.test(this.idCard);
+				 let nam = /^[\u4E00-\u9FA5]{2,4}$/.test(this.name);
+				 // let si = /\u4e00-\u9fa5/.test(this.site);
+				 // let texts =/\u4e00-\u9fa5/ .test(this.text);
+				 if (this.phone == "" || this.name == ""||this.idCard ==""||this.text == ""||this.idCard ==""||this.site == "") {
+				 	uni.showToast({
+				 		icon:'none',
+						title:'输入框不能为空!'
+				 	})
+				 }else if(!tel2){
+					 uni.showToast({
+					 	icon:'none',
+					 	title:'请输入正确的11位手机号码!'
+					 })
+				 }else if(!idcard){
+					 uni.showToast({
+					 	icon:'none',
+					 	title:'请输入正确的身份证号码!'
+					 })
+				 }else if(!nam){
+					 uni.showToast({
+					 	icon:'none',
+					 	title:'请您输入正确的名字!'
+					 })
+				 }
+				 // else if(!si){
+					//  uni.showToast({
+					//  	icon:'none',
+					//  	title:'请您输入正确的地址!'
+					//  })
+				 // }
+				 // else if(!texts){
+					//  uni.showToast({
+					//  	icon:'none',
+					//  	title:'请您输入正确格式!'
+					//  })
+				 // }
+			 }
 		}
 	}
 </script>
@@ -128,7 +174,7 @@
 				}
 				textarea{
 					width: 70%;
-					border: 1rpx solid blue;
+					height:130rpx;
 				}
 			}
 			.msg1{
@@ -147,9 +193,19 @@
 			}
 			view{
 				font-size: 24rpx;
-				padding: 20rpx 30rpx;
 			}
 		}
-		
+		button{
+			width:710rpx;
+			height:80rpx;
+			background:rgba(216,0,0,1);
+			border-radius:40rpx;
+			color:#FFFFFF;
+			text-align: 32rpx;
+			line-height: 80rpx;
+		}
+		.t3{
+			padding: 20rpx 30rpx;
+		}
 	}
 </style>
