8b3c2e8233ba2f1cfe1f5ec3a5894dbcdb0351d6
yulanyue <13433162425@163.com>
2019-12-19 15:31:05 +0800

    1219

--- pages/com_page/comment.vue
+++ pages/com_page/comment.vue
@@ -4,7 +4,17 @@
 					
 		</view>
 		<returns :titles='title'></returns>
-		
+		<view class="write">
+			<view class="w_title">觉得怎么样，打个星吧~</view>
+			<view class="start">
+				<image src="../../static/image/com_page/bigstart.png"></image>
+				<image src="../../static/image/com_page/bigstart.png"></image>
+				<image src="../../static/image/com_page/bigstart.png"></image>
+				<image src="../../static/image/com_page/bigstart.png"></image>
+				<image src="../../static/image/com_page/bigstart.png"></image>
+			</view>
+			<textarea value="" placeholder="请发表你的评论" />
+		</view>
 	</view>
 </template>
 
@@ -23,5 +33,22 @@
 </script>
 
 <style lang="scss">
-	
+	.write{
+		width: 100%;
+		height: 416rpx;
+		background:#F6F6F6 ;
+		.w_title{
+			font-size: 24rpx;
+			color:#666666;
+		}
+		.start{
+			display: flex;
+			image{
+				width: 41rpx;
+				height: 41rpx;
+				margin-right: 10rpx;
+			}
+		}
+		
+	}
 </style>
